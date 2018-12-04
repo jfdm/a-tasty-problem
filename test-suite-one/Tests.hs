655d9ad66a35776108ba0a0a096322b7bb82a566
Jan de Muijnck-Hughes <jan.deMuijnck-Hughes@glasgow.ac.uk>
2018-12-04 22:54:58 +0000

    Inital and final commit.

---
+++ test-suite-one/Tests.hs
@@ -0,0 +1,46 @@
+module Main where
+
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.Ingredients
+import           Test.Tasty.Options
+import           Test.Tasty.Runners
+import           Test.Tasty.Runners.AntXML
+import           Test.Tasty.Runners.Html
+
+--  ------------------------------------------------------------------- [ Main ]
+
+main :: IO ()
+main = defaultMainWithIngredients myIngredients $
+  testGroup "Private Internal Test Suite One"
+            [internalTests]
+
+myIngredients :: [Ingredient]
+myIngredients =  [composeReporters consoleTestReporter $ composeReporters htmlRunner antXMLRunner]
+              ++ defaultIngredients
+
+
+internalTests :: TestTree
+internalTests =
+  testGroup
+    "Testing Internals"
+    [exampleTests1, exampleTests2]
+
+
+exampleTests1 :: TestTree
+exampleTests1 =
+  testGroup
+    "Testing Command Execution 1"
+    [
+      testCase "Failing Test" (assertEqual "A" False True)
+    , testCase "Passing Test" (assertEqual "B" True True)
+    ]
+
+exampleTests2 :: TestTree
+exampleTests2 =
+  testGroup
+    "Testing Command Execution 2"
+    [
+      testCase "Failing Test" (assertEqual "C"False True)
+    , testCase "Passing Test" (assertEqual "D"True True)
+    ]
